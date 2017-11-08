3d14c37e8a51be4572d2861422cb96559707b92c
Marc Nussbaumer <marc.nussbaumer@stud.hslu>
2017-11-08 10:03:44 +0100

    Assets now not excluded in gitignore

---
+++ 03_Vortex_Tunnel_Prototype/Assets/CameraFarClip.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFarClip : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Camera cam = GetComponent<Camera>();
+        cam.farClipPlane = cam.farClipPlane * 100;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
