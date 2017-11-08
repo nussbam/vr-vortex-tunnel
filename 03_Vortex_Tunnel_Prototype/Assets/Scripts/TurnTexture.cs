34850ef3523da4382c77588168ebf9d47e7d1c23
Marc Nussbaumer <marc.nussbaumer@stud.hslu>
2017-11-08 14:10:11 +0100

    milestone presentation prototypes

---
+++ 03_Vortex_Tunnel_Prototype/Assets/Scripts/TurnTexture.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnTexture : MonoBehaviour {
+
+    Vector2 offset = Vector2.zero;
+    float speed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        offset.x = (offset.x + speed * Time.deltaTime);
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTextureOffset = offset;
+
+    }
+}
